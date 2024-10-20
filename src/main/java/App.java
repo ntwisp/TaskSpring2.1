7c09d5c47b2bc1e246aaf3a3536b5c3443d7e8cc
ntwisp <liangush@yandex.ru>
2024-10-20 06:15:56 +0300

    Task completed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,24 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanHW1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanHW2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+
+        if (cat1 == cat2) {
+            System.out.println("cat1 и cat2 имеют одинаковые ссылки");
+        } else {
+            System.out.println("cat1 и cat2 имеют разные ссылки");
+        }
+        if (beanHW1 == beanHW2) {
+            System.out.println("beanHW1 и beanHW2 имеют одинаковые ссылки");
+        } else {
+            System.out.println("beanHW1 и beanHW2 имеют разные ссылки");
+        }
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,13 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean(name = "cat")
+    @Scope(value = "prototype")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        cat.setMessage("Meow");
+        return cat;
+    }
+
 }
