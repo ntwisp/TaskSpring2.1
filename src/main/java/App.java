2b0a9c574106f7ed46d7d755760ae633cc4bb36c
ntwisp <liangush@yandex.ru>
2024-10-22 08:59:44 +0300

    Использовал тройной оператор Убрал лишние отступы

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,20 +9,9 @@ public class App {
                 (HelloWorld) applicationContext.getBean("helloworld");
         HelloWorld beanHW2 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-
         Cat cat1 = (Cat) applicationContext.getBean("cat");
         Cat cat2 = (Cat) applicationContext.getBean("cat");
-
-
-        if (cat1 == cat2) {
-            System.out.println("cat1 и cat2 имеют одинаковые ссылки");
-        } else {
-            System.out.println("cat1 и cat2 имеют разные ссылки");
-        }
-        if (beanHW1 == beanHW2) {
-            System.out.println("beanHW1 и beanHW2 имеют одинаковые ссылки");
-        } else {
-            System.out.println("beanHW1 и beanHW2 имеют разные ссылки");
-        }
+        System.out.println((cat1 == cat2) ? "cat1 и cat2 имеют одинаковые ссылки" : "cat1 и cat2 имеют разные ссылки");
+        System.out.println((beanHW1 == beanHW2) ? "beanHW1 и beanHW2 имеют одинаковые ссылки" : "beanHW1 и beanHW2 имеют разные ссылки");
     }
 }
